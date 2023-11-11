3545dd0a9e192e14ca52bdf33dc48e04a18d83ca
Margulan Daribayev <mdaribayev@mercari.com>
2023-11-11 18:21:22 +0900

    Launch

---
+++ halykoiy/halykoiyApp.swift
@@ -0,0 +1,17 @@
+//
+//  halykoiyApp.swift
+//  halykoiy
+//
+//  Created by mdaribayev on 2023/11/11.
+//
+
+import SwiftUI
+
+@main
+struct halykoiyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
